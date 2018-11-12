8b15e9daeaa696b2c3c8e630c960f8759bbb6a5f
Logan Willis <porkchopdonut@live.com>
2018-11-12 10:42:57 -0600

    App instantiation.

--- Django Sample/Django_Sample/urls.py
+++ Django Sample/Django_Sample/urls.py
@@ -1,21 +1,7 @@
-"""
-Definition of urls for Django_Sample.
-"""
-
 from django.conf.urls import include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+import HelloDjangoApp.views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', Django_Sample.views.home, name='home'),
-    # url(r'^Django_Sample/', include('Django_Sample.Django_Sample.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
