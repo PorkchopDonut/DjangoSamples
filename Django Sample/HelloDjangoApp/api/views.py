c307a1d74f226554229fe4e7a2139a8e27ae06aa
Logan Willis <porkchopdonut@live.com>
2018-11-13 20:34:22 -0600

    Incorporated searching functionality and read messages filter.

--- Django Sample/HelloDjangoApp/api/views.py
+++ Django Sample/HelloDjangoApp/api/views.py
@@ -1,10 +1,23 @@
 from HelloDjangoApp.models import Message
 from .serializers import MessageSerializer, UpdateMessageSerializer
+from django.db.models import Q
 from rest_framework import generics
 
 class ListAPIView(generics.ListAPIView):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        queryset = Message.objects.all()
+
+        read = self.request.query_params.get('read', None)
+        if read is not None:
+            queryset = queryset.filter(read=read)
+
+        search = self.request.query_params.get('search', None)
+        if search is not None:
+            queryset = queryset.filter(Q(sender__icontains=search) | Q(message__icontains=search) | Q(date__icontains=search))
+        
+        return queryset
 
 class UpdateAPIView(generics.UpdateAPIView):
     queryset = Message.objects.all()
